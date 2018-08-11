653e41c1367fcff38ffb9790b5a92c0e43d48cef
mass10 <mass10.github@gmail.com>
2018-08-11 17:28:44 +0900

    wip

---
+++ code/atcoder/PracticeA/perl/main.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+# coding: utf-8
+
+use strict;
+use utf8;
+
+sub _main {
+
+	binmode(STDIN, ':utf8');
+	binmode(STDOUT, ':utf8');
+	binmode(STDERR, ':utf8');
+
+	# 1st line
+	my $line = <STDIN>;
+	chomp($line);
+	my $a = int($line);
+
+	# 2nd line
+	$line = <STDIN>;
+	chomp($line);
+	my ($b, $c) = split(' ', $line);
+
+	# 3rd line
+	$line = <STDIN>;
+	chomp($line);
+	my $s = $line;
+
+	# summary
+	printf("a + b + c: [%s], s: [%s]\n", $a + $b + $c, $s);
+}
+
+_main();
