3d95060e1cb0611a8d37311b55843faee9ec7e59
mass10 <mass10.github@gmail.com>
2018-08-12 01:21:19 +0900

    wip

--- code/atcoder/PracticeA/rust/main.rs
+++ code/atcoder/PracticeA/rust/main.rs
@@ -1,44 +1,35 @@
-// extern crate atoi;
-
 fn parse_int(s: String) -> i32 {
 
 	return s.parse::<i32>().unwrap();
 }
 
+fn read_line() -> String {
+
+	let mut line = String::new();
+	if std::io::stdin().read_line(&mut line).is_err() {
+		return String::new();
+	}
+	let line = line.trim_right().to_string();
+	return line;
+}
+
 fn main() {
 
-	let stdin = std::io::stdin();
-
-	// 1st line
-	let mut line = String::new();
-	if stdin.read_line(&mut line).is_err() {
-		return;
-	}
-	let line = line.trim_right().to_string();
+	// ===== 1st line =====
+	let line = read_line();
 	let a = parse_int(line);
-	// println!("[TRACE] a: {:?}", a);
 
-	// 2nd line
-	let mut line = String::new();
-	if stdin.read_line(&mut line).is_err() {
-		return;
-	}
-	let line = line.trim_right().to_string();
+	// ===== 2nd line =====
+	let line = read_line();
 	let items = line.split(" ");
-	// let vec: Vec<&str> = split.collect();
 	let items: Vec<&str> = items.collect();
 
-	// println!("{:?}", items);
 	let b = parse_int(items[0].to_string());
 	let c = parse_int(items[1].to_string());
-	// println!("b: {:?}, c: {:?}", b, c);
 
-	// 3rd line
-	let mut line = String::new();
-	if stdin.read_line(&mut line).is_err() {
-		return;
-	}
-	let line = line.trim_right().to_string();
+	// ===== 3rd line =====
+	let line = read_line();
 
+	// ===== summary =====
 	println!("{:?} {}", a + b + c, line);
 }
